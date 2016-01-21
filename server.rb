4c08538c1b36b1d08fa4648f423413b75c0e9f2e
Jason Chan <jlc370@gmail.com>
2016-01-20 23:30:05 -0500

    Posts to topic and upvotes are go

--- server.rb
+++ server.rb
@@ -8,6 +8,7 @@ module Forum
   class Server < Sinatra::Base
     # session handling, must be enabled! to secure login instead of cookies
     enable :sessions
+    set :method_override,true
 
     @@db = PG.connect dbname: 'forum_dev'
 
@@ -37,7 +38,7 @@ module Forum
           session['user_id'] = returning_user.first['id'].to_i
           session['logged_in'] = true
           # binding.pry
-          erb :index
+          redirect '/'
         else
           # sends to ending page if password incorrect, notifies
           @password = true
@@ -57,7 +58,7 @@ module Forum
     end
 
     get '/' do
-      @top_posts = @@db.exec('SELECT * FROM topics LIMIT 3')
+      @top_topics = @@db.exec('SELECT * FROM topics LEFT JOIN users ON user_id = users.id ORDER BY upvotes DESC LIMIT 3')
       erb :index
     end
 
@@ -73,16 +74,21 @@ module Forum
       SQL
       # this user_id is just a var, but now they are tagged?
       session['user_id'] = new_user.first['id'].to_i
-      erb :index
+      redirect '/'
     end
 
     get '/topics' do
-      @topic_list = @@db.exec('SELECT * FROM topics')
+      # @topic_list = @@db.exec('SELECT * FROM topics LEFT JOIN users ON user_id = users.id')
+      @topic_list = @@db.exec('SELECT topics.*, users.name FROM topics, users WHERE topics.user_id = users.id')
+
       # binding.pry
       erb :topics
     end
 
-    get '/topic_thread' do
+    get '/topic_thread/:id' do
+      @this_topic = params[:id]
+      @topic_post = @@db.exec("SELECT topics.*, users.name FROM topics, users WHERE topics.id = #{@this_topic}")[0]
+      @post_list = @@db.exec("SELECT posts.*, users.name FROM posts, users WHERE posts.topics_id = #{@this_topic} AND user_id = users.id")
       erb :topic_thread
     end
     # Topic and Post routes and creation
@@ -103,12 +109,27 @@ module Forum
       redirect '/topics'
     end 
 
-    get '/regular_post' do
+    get '/regular_post/:id' do
+      @topic_id = params[:id]
       erb :regular_post
     end
 
-    post '/regular_post' do 
+    put '/update/:id' do
+      @up_id = params[:id].to_i
+      @@db.exec("UPDATE topics SET upvotes = upvotes + 1 WHERE id = #{@up_id}")
+      # binding.pry
+      redirect '/topics'
+    end
 
+    post '/regular_post' do 
+      @content = params["content"]
+      @t_id = params["topics_id"]
+      @op = session['user_id']
+      @@db.exec_params(<<-SQL,[ @content, @op, @t_id ])
+      INSERT INTO posts (content, user_id, topics_id)
+      VALUES ($1,$2,$3);
+      SQL
+      redirect '/topics'
     end 
 
     get '/end' do
